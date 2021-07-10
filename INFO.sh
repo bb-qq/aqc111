805892b20ef74a06d51eb1320cd897c66eb29563
bb-qq <bb-qq@users.noreply.github.com>
2021-07-10 16:11:17 +0900

    Added DSM 7 support.

--- INFO.sh
+++ INFO.sh
@@ -9,5 +9,29 @@ displayname="AQC111 driver"
 maintainer="bb-qq"
 arch="$(pkg_get_platform)"
 description="Driver for Aquantia AQC111U Based USB Ethernet Adapters."
+install_type="package"
+thirdparty="yes"
+
 [ "$(caller)" != "0 NULL" ] && return 0
+
+if [ "${PRODUCT_VERSION}" = "7.0" ]; then
+    os_min_ver="7.0-40000"
+    RUN_AS="package"
+else
+    RUN_AS="root"
+fi
+
+cat <<EOS > `dirname $0`/conf/privilege
+{
+    "defaults": {
+        "run-as": "${RUN_AS}"
+    }
+}
+EOS
+
+cat <<EOS > `dirname $0`/SynoBuildConf/depends
+[default]
+all="${PRODUCT_VERSION}"
+EOS
+
 pkg_dump_info
